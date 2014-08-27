7e733a68af0c136f8874b7a636fb48d64eb3b12f
Steve Ivy <steve@wallrazer.com>
2014-08-26 22:41:43 -0700

    first pass at some code. sort of works. kinda.

---
+++ boxpub/postutils.py
@@ -0,0 +1,82 @@
+import re
+import markdown
+import logging
+import yaml
+from yaml.scanner import ScannerError
+
+log = logging.getLogger(__name__)
+
+def split_markdown(md):
+    """
+    File content could have 0 or more yaml lines at the start
+    """
+    log.debug('split_markdown()')
+    log.debug(md)
+
+    md = md.strip()
+
+    meta_lines = []
+
+    # line-oriented should work better
+    # trim any lines with
+    md = re.sub("[ \t]+\n", "\n", md)
+
+    paras = re.split(
+        '\n\n|\r\n\r\n|\r\r',
+        md+'\n\n')  # whee, lots of linefeeds!
+
+    log.debug('paras:')
+    log.debug(paras)
+
+    meta_string = paras[0]  # pretty sure it's going to have this either way
+    log.debug('meta_string:')
+    log.debug(meta_string)
+
+    if meta_string:
+        try:
+            meta = yaml.load(meta_string)
+            content = '\n\n'.join(paras[1:])
+        except ScannerError, se:
+            log.error(se.message)
+            log.exception(se)
+            meta = {}
+        log.debug("STRIP META YAML: " + repr(meta))
+
+    else:
+        content = '\n\n'.join(paras)
+
+    content = content.strip()
+
+    log.debug("split_markdown() returning: " + repr((meta, content)))
+    return (meta, content)
+
+
+def process_markdown(path, md):
+    log.debug("process_markdown()")
+    meta, content = split_markdown(md)
+    log.warning('meta, content')
+    log.debug(meta)
+    #
+    # this can explode if yaml.Scanner cannot parse the meta
+    # -- we let it blow up and catch the error one level up
+    #
+    # if meta:
+    #     try:
+    #         meta = yaml.load(meta)
+    #     except ScannerError, se:
+    #         log.error(se.message)
+    #         log.exception(se)
+    #         meta = {}
+    #     log.debug("POST META YAML: " + repr(meta))
+
+    html = markdown.markdown(content, ['extra', 'headerid', 'codehilite'])
+
+    data = {
+        'path': path,
+        'meta': meta,
+        'markdown': content,
+        'html': html
+    }
+
+    log.info(data)
+    return data
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.md') as f:
+	README = f.read()
+
+with open('requirements.txt') as f:
+	REQUIREMENTS = f.read()
+	REQUIREMENTS = REQUIREMENTS.split('\n')
+
+setup(
+	name='boxpub',
+    version='0.1',
+    description='basic dropbox web publisher',
+    long_description=README,
+    author='Steve Ivy',
+    author_email='steveivy@gmail.com',
+    url='http://monkinetic.com',
+    packages=['boxpub'],
+    include_package_data=True,
+    install_requires=REQUIREMENTS
+	)
