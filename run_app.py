64aa476e87128b3cefbbe773637d1d6d6e7f2101
Steve Ivy <steve@wallrazer.com>
2014-08-29 17:26:09 -0700

    remove

--- run_app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-sys.dont_write_bytecode = True
-sys.path.append('./boxpub')
-sys.path.append('/etc/boxpub')
-
-from boxpub import boxpub
-boxpub.run(debug=True, host='0.0.0.0')
